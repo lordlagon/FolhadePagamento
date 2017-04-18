0d8042f4923296757d82319fd3d4bd927eaff87e
LoAguias <zarpelon091098@gmail.com>
2017-04-18 14:34:24 -0300

    Começo do programa

--- FolhadePagamento/View/Program.cs
+++ FolhadePagamento/View/Program.cs
@@ -12,6 +12,16 @@ namespace FolhadePagamento.View
     {
         static void Main(string[] args)
         {
+            string opcao = null;
+            Funcionario funcionario = new Funcionario();
+            FolhaPagamento folhaPagamento = new FolhaPagamento();
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(" -- ");
+            } while (!opcao.Equals("0"));
+
         }
     }
 }
