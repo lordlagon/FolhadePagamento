a2a93e0c534fc3e4399a0a2875f4f5bc0b1e8005
Andre Xavier <lordlagon@gmail.com>
2017-04-24 13:34:40 -0300

    folhapgto

--- FolhadePagamento/Model/FolhaPagamento.cs
+++ FolhadePagamento/Model/FolhaPagamento.cs
@@ -24,7 +24,7 @@ namespace FolhadePagamento.Model
 
         public override string ToString()
         {
-            return " Funcionario: " + Funcionario + " Mês: " + Mes + " Ano: " + Ano +  "\nSalário Bruto: " + SalarioBruto.ToString("C2") + "\n" + Salario;
+            return " Funcionario: " + Funcionario.Nome + " Mês: " + Mes + " Ano: " + Ano +  "\nSalário Bruto: " + SalarioBruto.ToString("C2") + "\n" + Salario;
         }
     }
 }
