308032b4422c6f2ab0d36a3ab5a4b38a0b371ce1
André Xavier <andre.macedo@aluno.positivo.edu.br>
2017-04-28 18:18:31 -0300

    Retirei nossos cpf dos dados

--- FolhadePagamento/DAL/Dados.cs
+++ FolhadePagamento/DAL/Dados.cs
@@ -11,38 +11,18 @@ namespace FolhadePagamento.DAL
     {
         public static void Inicializar()
         {
-            List<Funcionario> funcionarios = new List<Funcionario>
+           /* List<Funcionario> funcionarios = new List<Funcionario>
             {
                 new Funcionario()
                 {
-                    Nome = "André",
-                    Cpf = "009.061.419-40"
-                },
-                new Funcionario()
-                {
-                    Nome = "Gabriel",
-                    Cpf = "083.809.369-81"
-                },
-                new Funcionario()
-                {
-                    Nome = "Bruno",
-                    Cpf = "092.181.829-78"
-                },
-                new Funcionario()
-                {
-                    Nome = "Ale",
-                    Cpf = "116.205.589-80"
-                },
-                new Funcionario()
-                {
-                    Nome = "Diogo",
-                    Cpf = "088.408.549-08"
+                    //Retirei nossos CPFs do Programa
+                           
                 },
 
             };
             funcionarios.ForEach(x => FuncionarioDao.AdicionarFuncionario(x));
 
-
+*/
         }
     }
 }
